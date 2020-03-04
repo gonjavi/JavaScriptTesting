20b374881aa7057a9342dc4e3535dd0b4b1019ce
gonjavi <javiercaliescali@gmail.com>
2020-03-04 11:46:53 -0500

    capitilize function test passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 function capitalize(string) {
-
+  let f= '' ;
+  f = string.charAt(0).toUpperCase();
+  string = string.split('');
+  string.splice(0,1,f);
+  return string.join("");
 }
-
+ 
 module.exports = capitalize;
